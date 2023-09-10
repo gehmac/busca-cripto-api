0ae34d095a9ec4dd579586f28332b8385d375ec7
Geovanni <gmarcoscardoso@gmail.com>
2023-09-10 16:23:11 -0300

    refactor: test to deploy

--- src/controllers/currency/info/currency-info-controller.ts
+++ src/controllers/currency/info/currency-info-controller.ts
@@ -1,11 +1,10 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get } from '@nestjs/common';
 
 @Controller({ path: 'info/currency', version: '*' })
 export class CurrencyInfoController {
-
   @Get()
   async getCurrencyPrices() {
-    console.log("oiii");
-
+    console.log('oiii');
+    return 'oi';
   }
 }
